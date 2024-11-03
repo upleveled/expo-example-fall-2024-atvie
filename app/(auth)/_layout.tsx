b2ebdff0521809669afc0909dce6152a3ffe44cd
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-11-03 14:14:56 +0100

    Add Login as the initial screen name

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,6 +3,7 @@ import { Slot } from 'expo-router';
 export default function AuthLayout() {
   return (
     <Slot
+      initialRouteName="Login"
       screenOptions={{
         unmountOnBlur: true,
       }}
