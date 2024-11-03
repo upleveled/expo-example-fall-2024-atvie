bd97d6bd5eba88e7e416336a07dfa0aad12ae138
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-11-03 17:16:30 +0100

    Add comment for Slot component

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,6 +2,8 @@ import { Slot } from 'expo-router';
 
 export default function AuthLayout() {
   return (
+    // Renders all the pages in the (auth) group folder
+    // https://docs.expo.dev/router/layouts/#:~:text=the%20above%20example%2C-,Slot,-will%20render%20the
     <Slot
       initialRouteName="Login"
       screenOptions={{
