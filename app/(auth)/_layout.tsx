87eed2e8e727fe21d5cd23b9bf313889f64f65b9
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-11-03 20:39:02 +0100

    Refactor comment

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,7 +2,7 @@ import { Slot } from 'expo-router';
 
 export default function AuthLayout() {
   return (
-    // Render all the pages in the (auth) group folder
+    // Prevent header from showing on auth group pages
     // https://docs.expo.dev/router/layouts/#:~:text=the%20above%20example%2C-,Slot,-will%20render%20the
     <Slot initialRouteName="Login" />
   );
