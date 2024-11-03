2108e254c86b9ec0b984ef0d5e515c1bf097593b
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-11-03 20:03:14 +0100

    Update comment

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,7 +2,7 @@ import { Slot } from 'expo-router';
 
 export default function AuthLayout() {
   return (
-    // Renders all the pages in the (auth) group folder
+    // Render all the pages in the (auth) group folder
     // https://docs.expo.dev/router/layouts/#:~:text=the%20above%20example%2C-,Slot,-will%20render%20the
     <Slot
       initialRouteName="Login"
